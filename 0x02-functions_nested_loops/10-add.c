5ebef440b6ef93d482d1950dee0d815d300430ef
Chybozz <martinsonuchukwuchimdinyerem@gmail.com>
2023-09-12 23:44:21 -0700

    task 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * add - function that adds two numbers
+ *
+ * @val1: first value
+ *
+ * @val2: second value
+ *
+ * Description: to print the sum of two numbers
+ *
+ * Return: 0
+ */
+int add(int val1, int val2)
+{
+	int sum;
+
+	sum = val1 + val2;
+	return (sum);
+}
