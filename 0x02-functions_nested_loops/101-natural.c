215f2296bac468508079cfd6aa241594758bc371
Chybozz <martinsonuchukwuchimdinyerem@gmail.com>
2023-09-13 10:34:41 -0700

    task 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a, add = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 5 == 0) || (a % 3 == 0))
+			add += a;
+	}
+	printf("%d\n", add);
+	return (0);
+}
