d2ff8f28f1708e7ee7a1124345ec31466047ac9c
Chybozz <martinsonuchukwuchimdinyerem@gmail.com>
2023-09-12 22:21:06 -0700

    task 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Description: prints every minutes starting from 00:00 to 23:59.
+ *
+ */
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
