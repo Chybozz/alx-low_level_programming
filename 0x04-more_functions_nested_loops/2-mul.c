62b67c61112b4ac8a49e43eec745ca6bfb31c5ef
Chybozz <martinsonuchukwuchimdinyerem@gmail.com>
2023-09-15 00:41:49 -0700

    task multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * mul - function to multiply two numbers
+ *
+ * @a: first integer value
+ *
+ * @b: second integer value
+ *
+ * Return: (0) Success
+ *
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
