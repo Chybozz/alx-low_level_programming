4460e7078518de81bf75f0ab4633cf792263faf5
Chybozz <martinsonuchukwuchimdinyerem@gmail.com>
2023-10-24 12:47:54 -0700

    task function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
-	size_t i, max = new size;
+	size_t i, max = new_size;
 	char *oldp = ptr;
 
 	if (ptr == NULL)
