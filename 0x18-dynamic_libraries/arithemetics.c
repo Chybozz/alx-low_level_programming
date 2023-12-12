6d9f5941febeff605668b4d16b7e438cf8f0e38b
Chybozz <martinsonuchukwuchimdinyerem@gmail.com>
2023-12-11 18:32:54 -0800

    calling C functions from Python

---
+++ 0x18-dynamic_libraries/arithemetics.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add - addition of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: final answer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtract two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: final answer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div - divide two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: final answer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: final answer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - mod two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
